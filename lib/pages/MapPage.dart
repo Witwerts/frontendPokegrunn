dbe5b04c8f7d019f13025695adf5f09f3f6f9702
robobo1221 <robobo1221@gmail.com>
2024-01-23 19:11:41 +0100

    Fixed markers

--- lib/pages/MapPage.dart
+++ lib/pages/MapPage.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:pokegrunn/controllers/AchievementController.dart';
 import 'package:pokegrunn/controllers/LocationController.dart';
+import 'package:pokegrunn/controllers/NavigationController.dart';
+import 'package:pokegrunn/models/AchievementModel.dart';
 import 'package:pokegrunn/models/NavigationPageState.dart';
 import 'package:pokegrunn/widgets/Titlebar.dart';
+import 'package:provider/provider.dart';
 import '../models/NavigationPage.dart';
 
 class MapPage extends NavigationPage  {
@@ -23,14 +27,47 @@ class MapPage extends NavigationPage  {
 class MapPageState extends NavigationPageState {
   late LocationController mapController;
   bool followMe = false;
+  List<Marker> markerList = [];
 
 
   MapPageState(){
     mapController = LocationController(animationManager);
   }
 
+  void createMarkers(AchievementController achievementController) {
+    List<Marker> markers = [];
+
+    for (AchievementModel achievement in achievementController.achievements) {
+      double lon = double.parse(achievement.longitude!);
+      double lat = double.parse(achievement.latitude!);
+
+      markers.add(
+        Marker(
+          point: LatLng(lat, lon),
+          width: 60.0,
+          height: 60.0,
+          alignment: Alignment.topCenter,
+          child: const Icon(
+            Icons.location_on,
+            size: 60,
+            color:Colors.white,
+            shadows: [Shadow(offset: Offset(2.0, 2.0))],
+          )
+        )
+      );
+    }
+
+    setState(() {
+      markerList = markers;
+      print(markerList);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    AchievementController achievementController = Provider.of<AchievementController>(context);
+    createMarkers(achievementController);
+
     return Stack(
       children: [
         FlutterMap(
@@ -52,7 +89,8 @@ class MapPageState extends NavigationPageState {
                 marker: DefaultLocationMarker(),
                 markerDirection: MarkerDirection.top,
               ),
-            )
+            ),
+            MarkerLayer(markers: markerList)
           ],
         ),
         const Titlebar(title: "Achievements in de buurt"),
