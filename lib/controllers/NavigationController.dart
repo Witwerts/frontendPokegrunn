1f1f0c76f72fa61746d08a972217796ce344a8fb
robobo1221 <robobo1221@gmail.com>
2024-01-24 16:02:27 +0100

    merge

--- lib/controllers/NavigationController.dart
+++ lib/controllers/NavigationController.dart
@@ -137,7 +137,7 @@ class NavigationController extends ChangeNotifier {
 
   NavigationPage? getPage(String route) {
     if (pages.containsKey(route)) {
-      return pages[route]!;
+      return pages[route]!();
     }
 
     return null;
